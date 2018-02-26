21e22c97f19efc1704532f524721beff8008a70a
Miguel Ferrer <ingferrermiguel@gmail.com>
2018-02-26 08:40:27 -0500

    Add MANIFEST.in; Bump ver. 0.1.1

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+import os
 from setuptools import setup
 
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
 setup(name='facebookmarketing-python',
-      version='0.1',
+      version='0.1.1',
       description='API wrapper for Facebook written in Python',
+      long_description=read('README.md'),
       url='https://github.com/GearPlug/facebookmarketing-python',
       author='Miguel Ferrer',
       author_email='ingferrermiguel@gmail.com',
